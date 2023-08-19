3261b8ee8861480d49e41746726b462c908b19e0
Jan <jpetko1@gmail.com>
2023-08-19 19:02:10 +0200

    Improved game loop.

--- sources/main.cpp
+++ sources/main.cpp
@@ -68,12 +68,22 @@ struct Configuration
   int window_height = 800;
   HINSTANCE instance;
   int cmd_show;
+
+  float fps = 16;
 };
 
 Configuration read_config()
 {
   return Configuration();
 }
+
+struct Object
+{
+  Object(int id, float x, float y) : id(id), x(x), y(y) {}
+
+  int id = 0;
+  float x = 0.f, y = 0.f;
+};
 
 class Window
 {
@@ -139,22 +149,49 @@ public:
   void exec();
 
 private:
+  void addObject(float x, float y);
+
   std::unique_ptr<Window> win_;
+
+  std::chrono::duration<float, std::milli> frame_time_;
+
+  int object_unique_id_ = 0;
+  std::vector<Object> objects_;
 };
 
 void Game::init(Configuration const& config)
 {
   win_ = std::make_unique<Window>(config);
+  frame_time_ = std::chrono::milliseconds(1000) / config.fps;
+  addObject(config.window_width / 2.f, config.window_height / 2.f);
 }
 
 void Game::exec()
 {
   MSG msg = { };
+  auto prev_end_time = std::chrono::steady_clock::now();
+  auto prev_sleep_time = std::chrono::duration<float, std::milli>(0);
   while (GetMessage(&msg, NULL, 0, 0) > 0)
   {
-      TranslateMessage(&msg);
-      DispatchMessage(&msg);
+    const auto begin_time = std::chrono::steady_clock::now();
+    const auto prev_overshoot_time = begin_time - prev_end_time - prev_sleep_time;
+
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+
+    const auto end_time = std::chrono::steady_clock::now();
+    const auto body_duration = end_time - begin_time;
+
+    std::this_thread::sleep_for(frame_time_ - body_duration - prev_overshoot_time);
+
+    prev_end_time = end_time;
   }
+}
+
+void Game::addObject(float x, float y)
+{
+  objects_.emplace_back(object_unique_id_, x, y);
+  ++object_unique_id_;
 }
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
