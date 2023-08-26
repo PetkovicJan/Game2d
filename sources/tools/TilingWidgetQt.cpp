daf437884f936928c09736ae7745afa2eaefceae
Jan <jpetko1@gmail.com>
2023-08-26 10:48:22 +0200

    Added a container for tile data and a signal from TilesView to update it.

--- sources/tools/TilingWidgetQt.cpp
+++ sources/tools/TilingWidgetQt.cpp
@@ -1,52 +1,55 @@
 #include "TilingWidgetQt.hpp"
 
 #include <QHBoxLayout>
-#include <QGraphicsView>
 #include <QGraphicsScene>
 #include <QGraphicsRectItem>
-#include <QMouseEvent>
 
 #include <iostream>
 
-class TilesView : public QGraphicsView
+namespace detail
 {
-public:
-  explicit TilesView(int grid_width, int grid_height, int tile_size)
+  template<typename T>
+  T clamp(T val, T min, T max)
   {
-    auto tiles_scene = new QGraphicsScene();
-    this->setScene(tiles_scene);
-    this->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    this->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    return val < min ? min : (val > max ? max : val);
+  }
+}
 
-    for(int i = 0; i < grid_width; ++i)
-      for (int j = 0; j < grid_height; ++j)
-      {
-        const auto left = i * tile_size;
-        const auto top = j * tile_size;
-        QRectF tile_rect(left, top, tile_size, tile_size);
-        auto tile_item = tiles_scene->addRect(tile_rect);
-      }
-    }
+TilesView::TilesView(int grid_width, int grid_height, int tile_size)
+{
+  auto tiles_scene = new QGraphicsScene(this);
+  this->setScene(tiles_scene);
+  this->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+  this->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
-private:
-  void labelTileAt(QPoint pos)
-  {
-    if (auto item = this->itemAt(pos); item)
+  for(int i = 0; i < grid_width; ++i)
+    for (int j = 0; j < grid_height; ++j)
     {
-      static_cast<QGraphicsRectItem*>(item)->setBrush(QBrush(Qt::green));
+      const auto left = i * tile_size;
+      const auto top = j * tile_size;
+      QRectF tile_rect(left, top, tile_size, tile_size);
+      auto tile_item = tiles_scene->addRect(tile_rect);
     }
-  }
+}
 
-  void mousePressEvent(QMouseEvent* event) override
+void TilesView::labelTileAt(QPoint pos)
+{
+  if (auto item = this->itemAt(pos); item)
   {
-    labelTileAt(event->pos());
+    static_cast<QGraphicsRectItem*>(item)->setBrush(QBrush(Qt::green));
+    emit tileLabeledAt(pos);
   }
+}
 
-  void mouseMoveEvent(QMouseEvent* event) override
-  {
-    labelTileAt(event->pos());
-  }
-};
+void TilesView::mousePressEvent(QMouseEvent* event)
+{
+  labelTileAt(event->pos());
+}
+
+void TilesView::mouseMoveEvent(QMouseEvent* event)
+{
+  labelTileAt(event->pos());
+}
 
 TilingWidget::TilingWidget(QWidget* parent) : QWidget(parent)
 {
@@ -57,6 +60,8 @@ void TilingWidget::setConfiguration(int window_width, int window_height, int til
   const auto grid_width = window_width / tile_size;
   const auto grid_height = window_height / tile_size;
 
+  tiles_.resize(grid_width * grid_height);
+
   const auto actual_window_width = grid_width * tile_size;
   const auto actual_window_height = grid_height * tile_size;
 
@@ -66,4 +71,18 @@ void TilingWidget::setConfiguration(int window_width, int window_height, int til
   auto top_layout = new QHBoxLayout();
   this->setLayout(top_layout);
   top_layout->addWidget(tiles_view);
+
+  QObject::connect(tiles_view, &TilesView::tileLabeledAt, 
+    [this, grid_width, grid_height, tile_size](QPoint pos)
+    {
+      // Compute grid 2d index.
+      const auto grid_x = detail::clamp(pos.x() / tile_size, 0, grid_width - 1);
+      const auto grid_y = detail::clamp(pos.y() / tile_size, 0, grid_height - 1);
+
+      // Transform into linear index.
+      const auto lin_index = grid_y * grid_width + grid_x;
+
+      // Save.
+      tiles_.at(lin_index).labeled = true;
+    });
 }
--- sources/tools/TilingWidgetQt.hpp
+++ sources/tools/TilingWidgetQt.hpp
@@ -1,6 +1,13 @@
 #pragma once
 
 #include <QWidget>
+#include <QGraphicsView>
+#include <QMouseEvent>
+
+struct Tile
+{
+  bool labeled = false;
+};
 
 class TilingWidget : public QWidget
 {
@@ -10,4 +17,24 @@ public:
   explicit TilingWidget(QWidget* parent = nullptr);
 
   void setConfiguration(int window_width, int window_height, int tile_size);
+
+private:
+  std::vector<Tile> tiles_;
+};
+
+class TilesView : public QGraphicsView
+{
+  Q_OBJECT
+
+public:
+  explicit TilesView(int grid_width, int grid_height, int tile_size);
+
+signals:
+  void tileLabeledAt(QPoint pos);
+
+private:
+  void labelTileAt(QPoint pos);
+
+  void mousePressEvent(QMouseEvent* event) override;
+  void mouseMoveEvent(QMouseEvent* event) override;
 };
